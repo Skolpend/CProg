f0c531a2805eaf7fa260b7faa9c94a9a29c6282d
Skolpend <y.a.klimov@mail.ru>
2020-10-07 13:13:25 +0300

    lab2.0

---
+++ lab 2/lab2,Zad1.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void printTableHeader()
+{
+    cout << "+-----+-----+" << endl;
+    cout << "|  x  |  y  |" << endl;
+}
+
+void printTableRow(float x, float y)
+{
+    cout << "+-----+-----+" << endl;
+    cout << "|";
+    printf("%4.2f", x);
+    cout << " |";
+    printf("%5.2f", y);
+    cout << "|" <<endl;
+}
+
+void printTableEnd()
+{
+    cout << "+-----+-----+" << endl;
+}
+
+float f(float x)
+{
+    return x * x;
+}
+
+void getTableFor(float a, float b, float count)
+{
+    float step = (b - a) / count;
+    printTableHeader();
+    for (float x = a; x < b; x += step)
+    {
+        float y = x * x;
+        printTableRow(x, y);
+    }
+    printTableEnd();
+}
+
+void getTableWhile(float a, float b, float count)
+{
+    float step = (b - a) / count;
+    float x = a;
+    printTableHeader();
+    while (x < b)
+    {
+        float y = x * x;
+        printTableRow(x, y);
+        x += step;
+    }
+    printTableEnd();
+}
+
+void getTableDoWhile(float a, float b, float count)
+{
+    float step = (b - a) / count;
+    float x = a;
+    printTableHeader();
+    do
+    {
+        float y = x * x;
+        printTableRow(x, y);
+        x += step;
+    } while (x < b);
+    printTableEnd();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float a, b, count;
+    cout << "Введите коардинату x1 : ";
+    cin >> a;
+    cout << "Введите коардинату x2 : ";
+    cin >> b;
+    cout << "Введите число шагов : ";
+    cin >> count;
+    getTableFor(a, b, count);
+    getTableWhile(a, b, count);
+    getTableDoWhile(a, b, count);
+    return 0;
+}
---
+++ lab1/Лаб1, Зад2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+float f1(float x, float y)
+{
+    return y - (2 * x + 2);
+}
+
+float f2(float x, float y)
+{
+    return y - (0.5 * x - 1);
+}
+
+float f3(float x, float y)
+{
+    return y - (-x + 2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << " Первое уровнение прямой: у = 2 * х + 2 " << endl;
+    cout << " Второе уровнение прямой: у = 0.5 * х - 1 " << endl;
+    cout << " Третье уровнение прямой: у = -х + 2 " << endl;
+    float x, y;
+    cout << " Введите коардинату x ";
+    cin >> x;
+    cout << " Введите коардинату y ";
+    cin >> y;
+    if (f1(x, y) != 0 && f2(x, y) != 0 && f3(x, y) != 0)
+    {
+        if (f1(x, y) < 0 && f2(x, y) > 0 && f3(x, y) > 0)
+            cout << 1 << " Область " << endl;
+        else if (f1(x, y) < 0 && f2(x, y) < 0 && f3(x, y) > 0)
+            cout << 2 << " Область " << endl;
+        else if (f1(x, y) < 0 && f2(x, y) < 0 && f3(x, y) < 0)
+            cout << 3 << " Область " << endl;
+        else if (f1(x, y) > 0 && f2(x, y) < 0 && f3(x, y) < 0)
+            cout << 4 << " Область " << endl;
+        else if (f1(x, y) > 0 && f2(x, y) > 0 && f3(x, y) < 0)
+            cout << 5 << " Область " << endl;
+        else if (f1(x, y) > 0 && f2(x, y) > 0 && f3(x, y) > 0)
+            cout << 6 << " Область " << endl;
+        else
+            cout << 7 << " Область " << endl;
+    }
+    else if (f1(x, y) == 0 && f2(x, y) == 0 && f3(x, y) != 0)
+        cout << "На пересичении 1 и 2 прямой" << endl;
+    else if (f1(x, y) != 0 && f2(x, y) == 0 && f3(x, y) == 0)
+        cout << "На пересичении 2 и 3 прямой" << endl;
+    else if (f1(x, y) == 0 && f2(x, y) != 0 && f3(x, y) == 0)
+        cout << "На пересичении 1 и 3 прямой" << endl;
+    else if (f1(x, y) == 0 && f2(x, y) != 0 && f3(x, y) != 0)
+        cout << "На 1 прямой" << endl;
+    else if (f1(x, y) != 0 && f2(x, y) == 0 && f3(x, y) != 0)
+        cout << "На 2 прямой" << endl;
+    else
+        cout << "На 3 прямой" << endl;
+    return 0;
+}
